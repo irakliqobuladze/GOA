aaf370af9f727f9367272031fa65923c4886f44f
irakliqobuladze <irali.qobuladze@gmail.com>
2024-05-28 00:25:14 +0400

    commit

---
+++ level 03/homework/homework.py
@@ -0,0 +1,19 @@
+name = "irakli"
+surname = "qobuladze"
+height = "1.80"
+age = "16"
+weight = "80"
+country = "georgia"
+city = "batumi"
+study = "Goa"
+school = "N=15"
+best_academy = "Goa is best"
+
+print("my name is " , name)
+print("my surname is " , surname)
+print("my height is " = height)
+print("my age is " = age)
+
+
+
+
