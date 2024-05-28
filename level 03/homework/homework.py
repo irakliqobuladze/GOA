df690f20b5aba746fc5e9b65862b9c53d8e51682
irakliqobuladze <irali.qobuladze@gmail.com>
2024-05-29 00:21:59 +0400

    commit

--- level 03/homework/homework.py
+++ level 03/homework/homework.py
@@ -6,14 +6,17 @@ weight = "80"
 country = "georgia"
 city = "batumi"
 study = "Goa"
-school = "N=15"
+school = "N=15 public school"
 best_academy = "Goa is best"
 
 print("my name is " , name)
 print("my surname is " , surname)
-print("my height is " = height)
-print("my age is " = age)
-
-
-
+print("my height is" , height)
+print("my age is" , age)
+print("my weight is" , weight)
+print("the country where i live is" , country)
+print("the city where i live" , city)
+print("the academy where i study is" , study)
+print("the number of school where i study is" , school)
+print("for me best_academy is" , best_academy)
 
