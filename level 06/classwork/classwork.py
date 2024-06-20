bcd36f362279c5149ac6085062420f74fb083910
irakliqobuladze <irali.qobuladze@gmail.com>
2024-06-20 22:33:17 +0400

    commit

--- level 06/classwork/classwork.py
+++ level 06/classwork/classwork.py
@@ -14,6 +14,11 @@ print(num1 * num2)
 print(num1 / num2)
 
 
-# 3)
+# 3
 
-
+name = str(input("enter your name: "))
+last_name = str(input("enter your last_name: "))
+age = str(input("enter your age: "))
+print("name is " + "" + name)
+print("last_name is " + "" + last_name)
+print("age is " + "" + age)
