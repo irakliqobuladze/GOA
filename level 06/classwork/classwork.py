e8f737a66bfa2e20d73e82a2297408fdf25a9fa6
irakliqobuladze <irali.qobuladze@gmail.com>
2024-06-20 21:30:12 +0400

    commit

---
+++ level 06/classwork/classwork.py
@@ -0,0 +1,17 @@
+# 1) 1) შემოატანინეთ მომხარებელს სახელი და გამოიტანეთ წინადადება: your name is... 
+
+name = input("enter your name")
+print(f"your name is {name}")
+
+# 2) შემოატანინეთ მომხმარებელს 2 რიცხვი და გააკეთეთ არიტმეტიკული ოპერაციები (+, - ,*,/) 
+
+num1= int(input("enter first number:  "))
+num2 = int(input("enter second num "))
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+# 3)
