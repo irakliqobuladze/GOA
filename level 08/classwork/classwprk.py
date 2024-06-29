1003ec5412d354b3f53d5576a00fd7f1c7d7c1fd
irakliqobuladze <irali.qobuladze@gmail.com>
2024-06-29 22:34:53 +0400

    commit

---
+++ level 08/classwork/classwprk.py
@@ -0,0 +1,24 @@
+print(20 < 21) 
+print(9 < 18)
+print(5 < 2)
+print(100 > 50)
+print(25 > 10)
+print(40 > 59)
+print(7 == 7)
+print(30 == 29)
+print(2 == 1)
+print(7 != 4)
+print(80 != 80)
+print(46 != 26)
+print(9 <= 9)
+print(78 <= 40)
+print(4 <= 4)
+print(90 >= 90)
+print(48 >= 0)
+print(0 >= 0)
+
+
+soil_moisture = 50
+print(soil_moisture < 70)
+
+
