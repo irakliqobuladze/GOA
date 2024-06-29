39c510ab2b9ae5ff0ad35aaf72b961961cf49323
irakliqobuladze <irali.qobuladze@gmail.com>
2024-06-30 01:19:56 +0400

    commit

--- level 06/classwork/classwork.py
+++ level 06/classwork/classwork.py
@@ -6,11 +6,3 @@ print(f"your name is {name}")
 
 
 
-# 3
-
-name = str(input("enter your name: "))
-last_name = str(input("enter your last_name: "))
-age = str(input("enter your age: "))
-print("name is " + "" + name)
-print("last_name is " + "" + last_name)
-print("age is " + "" + age)
