cbab3caa214e363fdd8b9cbc2d0d220538835361
irakliqobuladze <irali.qobuladze@gmail.com>
2025-03-28 23:29:04 +0400

    commit

---
+++ level 45/homework/script.js
@@ -0,0 +1,8 @@
+function computer(brand,){
+    this.brand = brand;
+    }
+
+const Computer1 = new computer1("msi");
+const Computer2 = new Computer2("asus");
+
+console.log(computer2.brand);
---
+++ level 45/homework/script2.js
@@ -0,0 +1,8 @@
+function keyboard(color,){
+    this.color = color;
+    }
+
+const keyboard1 = new keyboard1("green");
+const keyboard2 = new keyboard2("blue");
+
+console.log(keyboard1.color);
